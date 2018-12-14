e55d2f454c0f113089593041a9dae0c282b562bf
Thomas Peiselt <pi@kulturguerilla.org>
2018-12-14 14:01:11 +0100

    removed useless tests.
    remains from the initial template.

--- tasty/TastyTests.hs
+++ tasty/TastyTests.hs
@@ -1,19 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Test.Tasty
-import Test.Tasty.Hspec (testSpec)
-import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck
-import Lib (function1)
-import TastyHspec (rootSpec)
-import StatisticsTests
-import Chapter04.LinearAlgebra.ListBasedVectorProperties
-import Data.Complex (Complex)
+import           Chapter04.LinearAlgebra.ListBasedVectorProperties
+import           Data.Complex                                      (Complex)
+import           Lib                                               (function1)
+import           StatisticsTests
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
 
 main :: IO ()
 main = do
-  s <- testSpec "hspec tests" rootSpec
-  let tests = testGroup "all tests" [ statsTests, hunitTests, linearAlgebraTests, s ]
+  let tests = testGroup "all tests" [ statsTests, hunitTests, linearAlgebraTests ]
   defaultMain tests
 
 currentTests :: IO ()
